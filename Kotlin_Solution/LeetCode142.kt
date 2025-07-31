d6c7cc270b75f99e028ab89c5b725aac8d0b728a
Tran Quang Ha <tranquangha2006@gmail.com>
2025-07-31 16:33:59 +0700

    LeetCode 142 Medium

--- Kotlin_Solution/LeetCode142.kt
+++ Kotlin_Solution/LeetCode142.kt
@@ -1,20 +1,20 @@
 /*
     Problem - 142. Linked List Cycle II (Medium)
 
-    Goal:
+    🎯Goal:
     - Return the node where the cycle begins.
     - If there is no cycle, return null.
 
     Input: head = [3,2,0,-4], pos = 1
     Output: tail connects to node index 1 (node with value 2)
 
-    Techniques:
-    1. Floyd’s Cycle Detection (Two Pointers)
+    🚀Techniques:
+    ✅ 1. Floyd’s Cycle Detection (Two Pointers)
        - Time: O(n), Space: O(1)
        - Step 1: Use slow and fast pointers to detect cycle
        - Step 2: Reset one pointer to head, move both one step at a time until they meet → entry point of cycle
 
-    2. HashSet
+    ✅ 2. HashSet
        - Time: O(n), Space: O(n)
        - Use a HashSet to store visited nodes
        - If a node is already in the set → start of cycle
